f04615e8bc781b29922f1404fb0bced9657a3798
Emma <seoyoon7011@gmail.com>
2017-12-15 10:57:55 +1100

    add some extensions

--- index.js
+++ index.js
@@ -2,34 +2,105 @@ var row1 = document.querySelectorAll('.row1');
 var row2 = document.querySelectorAll('.row2');
 var row3 = document.querySelectorAll('.row3');
 
-var a = document.querySelectorAll('.a');
-var b = document.querySelectorAll('.b');
-var c = document.querySelectorAll('.c');
+var col1 = document.querySelectorAll('.col1');
+var col2 = document.querySelectorAll('.col2');
+var col3 = document.querySelectorAll('.col3');
 
 var cross = document.querySelectorAll('.cross');
 var slash = document.querySelectorAll('.slash');
 
-var span = document.querySelector('span');
-var button = document.querySelector('button');
+var restart = document.querySelector('.restart');
+var newgame = document.querySelector('.newGame');
+
+var score1 = document.querySelector('.score1');
+var score2 = document.querySelector('.score2');
+
+var round = document.querySelector('.round');
+var boxes = document.querySelectorAll('.box');
+var second = document.querySelector('.second');
+
+var player1 = document.querySelector('.player1');
+var player2 = document.querySelector('.player2');
+var overlay = document.querySelector('.overlay');
+var body = document.querySelector('body');
+var winner1 = document.querySelector('.winner1');
+var winner2 = document.querySelector('.winner2');
 
 
-var n = 1;
-function filltheBox() {
+var colorNumber=1;
+var count = 0;
+var run;
+var test;
+var startColor;
+var draw= 0;
+var numR = 0;
+var numB = 0;
+var scoreForRed = 0;
+var scoreForBlue = 0;
 
-    if(n % 2 !== 0) {
-      this.classList.add('red');
-    }else {
-      this.classList.add('blue');
-    }
-    n++;
-    winner();
+function colorSelectRed() {
+  startColor = player1.getAttribute('data-color');
+  console.log(startColor);
+  colorNumber=1;
+  return startColor;
+}
+function colorSelectBlue() {
+  startColor = player2.getAttribute('data-color');
+  colorNumber=2;
+  console.log(startColor);
+  return startColor;
 }
 
-var arr = [row1,row2,row3,a,b,c,cross,slash];
-function winner() {
+function filltheBox(event) {
 
-    for (var i = 0; i < arr.length; i++) {
-       arr[i].forEach(checking);
+    clearInterval(run)
+    count = 0;
+    second.textContent = count;
+
+    run = setInterval(function() {
+    count++;
+    console.log(count);
+    second.textContent = count;
+    },1000)
+
+    var check1 = this.classList.contains('red');
+    var check2 = this.classList.contains('blue');
+    console.log(check1);
+
+    if(!check1 && !check2) {
+       if(colorNumber % 2 !== 0) {
+          this.classList.add('red');
+          draw++;
+          second.style.backgroundColor = 'blue';
+        }else {
+          this.classList.add('blue');
+          draw++;
+          second.style.backgroundColor = 'red';
+
+         }
+           colorNumber++;
+
+        }
+        win();
+    if(draw == 9) {
+      clearInterval(run);
+      second.textContent = 0;
+      second.style.backgroundColor = 'white';
+        overlay.style.display = "block"
+    }
+
+}
+
+function removeDraw() {
+  overlay.style.display= "none"
+  reset();
+}
+
+var matchingArr = [row1,row2,row3,col1,col2,col3,cross,slash];
+function win() {
+
+    for (var i = 0; i < matchingArr.length; i++) {
+       matchingArr[i].forEach(checking);
            if(numR<3 || numB<3) {
             numR=0;
             numB=0;
@@ -37,37 +108,77 @@ function winner() {
     }
 }
 
-var numR = 0;
-var numB = 0;
-
 var checking = function(elem) {
 
       if(elem.classList.contains('red')) {
         numR++;
       }else if(elem.classList.contains('blue')) {
         numB++;
-      }else {
-        numR = 0;
-        numB = 0;
       }
 
       if(numR == 3) {
-        span.textContent = "RED WINS!"
+        console.log(numR);
+        winner1.style.color = 'red';
+        scoreForRed++;
+        score1.textContent = scoreForRed;
+        boxes.forEach(function(box) {
+          box.removeEventListener('click', filltheBox)});
+        clearInterval(run);
+        second.textContent = 0;
+        second.style.backgroundColor = 'white';
       }else if(numB == 3) {
-        span.textContent = "BLUE WINS!"
+        winner2.style.color = 'blue';
+        scoreForBlue++;
+        score2.textContent = scoreForBlue;
+        boxes.forEach(function(box) {
+          box.removeEventListener('click', filltheBox)});
+          clearInterval(run)
+          second.textContent = 0;
+          second.style.backgroundColor = 'white';
       }
 }
 
+var roundNum = 1;
+
 function reset() {
+  winner1.style.color = 'white';
+  winner2.style.color = 'white';
+  draw = 0;
+  boxes.forEach(function(box) {
+    box.classList.remove('red','blue')
+  });
+
+  console.log(colorNumber);
+  roundNum++
+  round.textContent = roundNum;
+  boxes.forEach(function(box) {
+    box.addEventListener('click', filltheBox);
+    clearInterval(run);
+    second.textContent = 0;
+    second.style.backgroundColor = 'white';
+  })
+  if(startColor == 'red' || startColor == undefined) {
+    colorNumber=1;
+  }else {
+    colorNumber=2
+  }
+
+  if(startColor) {
+  player1.removeEventListener('click', colorSelectRed);
+  player2.removeEventListener('click', colorSelectBlue);
+  }
+
+}
+
+function reload() {
   location.reload();
 }
-var boxes = document.querySelectorAll('.box');
+
+
+restart.addEventListener('click', reset);
+newgame.addEventListener('click', reload);
+player1.addEventListener('click', colorSelectRed);
+player2.addEventListener('click', colorSelectBlue);
 boxes.forEach(function(box) {
-  box.addEventListener('click', filltheBox);
-  // var i = 1;
-  // box.classList.add('box'+i);
-  // i++;
-})
-
-
-button.addEventListener('click', reset);
+  box.addEventListener('click', filltheBox)});
+overlay.addEventListener('click', removeDraw);
